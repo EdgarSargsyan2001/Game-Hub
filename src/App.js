6c40b0922334bd26da57cff4e3bd879a8d36ad85
TUMO Labs Student <tumolabsstudent@TUMOs-iMac.local>
2022-04-12 21:41:09 +0400

    themeProvider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // Lilit
 import { onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState ,createContext} from "react";
+import { useEffect, useState, createContext } from "react";
 import { Route, Routes } from "react-router-dom";
-import { auth } from './firebase'
+import { auth } from "./firebase";
 
 import Register from "./components/Header/LoginRegister/password/Register";
 import Login from "./components/Header/LoginRegister/login/Login";
@@ -12,42 +12,37 @@ import Layout from "./components/Layaouts/Layouts";
 import Home from "./components/Home";
 
 import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./Theme/theme";
 
-export const hasAccaunt = createContext()
-
-
-
+export const hasAccaunt = createContext();
 
 function App() {
-  
-  const [hasUser,setHasUser] = useState(false)
-  
-  
-  useEffect(()=>{
-    
-    onAuthStateChanged( auth,(user) => user ? setHasUser(user) : setHasUser(user) )
+  const [hasUser, setHasUser] = useState(false);
 
-    
-  },[])
-
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) =>
+      user ? setHasUser(user) : setHasUser(user)
+    );
+  }, []);
 
   return (
     <div className="App">
-      <hasAccaunt.Provider value={{hesAccaunt:hasUser,setHasAccount:setHasUser}}>
-       <Routes>
-
-        <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element ={<Login/>}/>
-          <Route path="/register" element ={<Register/>}/>
-          <Route path="/leaderpage" element={<LeaderPage/>}/>
-          <Route path="games/:gameName" element={<SingleGame/>}/>
-          
-        </Route>
-
-       </Routes>
-      </hasAccaunt.Provider>
-
+      <ThemeProvider theme={theme}>
+        <hasAccaunt.Provider
+          value={{ hesAccaunt: hasUser, setHasAccount: setHasUser }}
+        >
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/leaderpage" element={<LeaderPage />} />
+              <Route path="games/:gameName" element={<SingleGame />} />
+            </Route>
+          </Routes>
+        </hasAccaunt.Provider>
+      </ThemeProvider>
     </div>
   );
 }
